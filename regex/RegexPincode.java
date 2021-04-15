612581b55244d4816e2647043cffaa689a411619
vinod699 <kosarajuvinod9999@gmail.com>
2021-04-15 17:25:04 +0530

    uc2pincode

--- regex/RegexPincode.java
+++ regex/RegexPincode.java
@@ -1,34 +1,67 @@
+
 package regex;
-   import java.util.Scanner;
-        import java.util.regex.Pattern;
-        import java.util.regex.Matcher;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 
 public class RegexPincode{
 
-    public static void main(String args[]) {
-        System.out.println("WELCOME TO JAVA REGEX - VALIDATING A PINCODE");
-        System.out.println("ENTER A VALID PINCODE : ");
+    public static void validatePincode()
+    {
+        System.out.println("ENTER A VALID PINCODE: ");
+        System.out.println("*It is a 6 digits long code\n*It cannot begin with a special characters : [$&+,:;=?@#|'<>.^*()%!-] \n*It cannot begin with a alphabets : [A-Za-z]\n");
+
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
-        String regex = "\\d";
+        int length=input.length();
+        String regex = "[0-9]{6}";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(input);
 
-        int count=0;
-        while(matcher.find())
+        if(length == 6)
         {
-            count++;
+            System.out.println("PINCODE is 6 digit long");
+            char result=input.charAt(0);
+            System.out.println("PINCODE starts with :"+result);
+            //Converting char to string
+            String firstdigit=Character.toString(result);
+
+            //Pattern for Special Characters
+
+            String specialCharacters = "[$&+,:;=?@#|'<>.^*()%!-]";
+
+            //Pattern for alphabets
+            String alphabets = "[a-zA-Z]";
+
+            if(firstdigit.matches(specialCharacters))
+            {
+                System.out.println("PINCODE should not start with a special character - INVALID PINCODE");
+
+            }
+            else if (firstdigit.matches(alphabets))
+            {
+                System.out.println("PINCODE should not start with a alphabet - INVALID PINCODE");
+
+            }
+            else
+            {
+                System.out.println("PINCODE starts with a digit - VALID PINCODE");
+            }
+
         }
-        if(count == 6)
-            System.out.println("YOU HAVE ENTERED A VALID PINCODE");
         else
-            System.out.println("PLEASE ENTER A 6-DIGIT PINCODE");
+            System.out.println("PINCODE should be six digit long - INVALID PINCODE");
 
 
+    }
 
+
+    public static void main(String args[]) {
+        System.out.println("WELCOME TO JAVA REGEX - VALIDATING A PINCODE");
+        validatePincode();
 
     }
 }
